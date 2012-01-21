466808b3570a7280271e00051c0ae6f4c07aedad
jdkoreclipse <jdkoreclipse@gmail.com>
2012-01-21 12:10:09 -0500

    clean up jdkbot in preparation for 2.0

--- JDKBOT.sh
+++ JDKBOT.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #Pull config from file.
-DATE=$(date +"%m-%d-%Y")
+DATE=$( date +%m-%d-%Y_%H-%M-%S)
 HOST=$"jdkoreclipse.net"
 UPDIR=$"mirror/kernel/tuna"
 PACKAGE=$"jdkx_tuna_nightly_$DATE.zip"
-LCD=$"~/out/target/product/toro"
-ServerCD=$"public_html/mirror/rom/tuna"
+LCD=$"~/android/system/out/target/product/toro"
+ServerCD=$"public_html/mirror/rom/toro"
 
  echo "
  === Welcome to JDBOT! Build has initiated successfully! ===
 "
 cd ~/android/system
 repo sync -j4
-make installclean
 . build/envsetup.sh && lunch 1
-make otapckage -j16
+make otapackage -j16
 cd $LCD 
 echo " 
 === Packing files. ===
 "
-cp ~/android/system/out/target/product/toro/jdk_toro-ota-eng.jdkoreclipse.zip  ~/out/target/product/toro/$PACKAGE
+rm ~/android/system/out/target/product/toro/$PACKAGE
+cp ~/android/system/out/target/product/toro/jdk_toro-ota-eng.jdkoreclipse.zip  ~/android/system/out/target/product/toro/$PACKAGE
 
 echo " 
 === Uploading. ===
@@ -36,7 +36,7 @@ EOT
 
 ftp -n -v $HOST << EOT
 ascii
-user <user> <pass>
+user <username> <password>
 prompt
 cd $ServerCD
 lcd $LCD 
@@ -46,5 +46,3 @@ echo "
 === DONE! ===
 "
 exit
-
-
