5a392c2ca17a7abdc231d0d6bac38fc3060019d9
jdkoreclipse <jdkoreclipse@gmail.com>
2012-01-24 15:20:26 -0500

    use MST for modver, not UTC (doesnt matter to me, im EST, but the zip is named in MST because thats where my build server is)

--- products/common.mk
+++ products/common.mk
@@ -34,7 +34,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
 
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_MST_DATE=$(shell date +"%s")
 
 # Blobs common to all devices
 PRODUCT_COPY_FILES += \
